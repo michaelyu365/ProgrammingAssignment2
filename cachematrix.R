8e5aeb7b361eedc7f38f54e4f22457cce406c4d4
michaelyu365 <michaelyu365@gmail.com>
2018-05-03 01:28:59 -0400

    Final submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a set of functions that will be used to create & test for cached values
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL                               ## assigns m in the Global Environment to NULL
+        set <- function(y) {                    ## creates a "set" closure that sets the global value of x equal to y
+                x <<- y
+                m <<- NULL
+                }
+        get <- function() x                     ## creates a "get" closure that returns the local value of x
+        setinverse <- function(inverse) m <<- inverse    ## creates a "setinverse" closure that assigns m in the Global Environment to the inverse
+        getinverse <- function() m                       ## creates a "getinverse" closure that returns the local value of m
+        list(set = set, get = get,                       ## returns a list of 4 functions, set, get, setinverse, getinverse
+                setinverse = setinverse,
+                getinverse = getinverse)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
